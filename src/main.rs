5465087f796da6fb36ad7e65f13d1ae0f017a887
Cozmickiwi <sam.rogers.kiwi@gmail.com>
2024-01-13 02:46:02 +1300

    create single threaded server

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{prelude::*, BufReader},
     net::{TcpListener, TcpStream},
 };
@@ -13,11 +14,22 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
-    let http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|r| r.unwrap())
-        .take_while(|a| !a.is_empty())
-        .collect();
-
-    println!("Request: {:#?}", http_request);
+    let request_line = buf_reader.lines().next().unwrap().unwrap();
+    let (status_line, filename) = if request_line == "GET / HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "frontend/index.html")
+    } else if request_line == "GET /styles.css HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "frontend/styles.css")
+    } else if request_line == "GET /script.js HTTP/1.1" {
+        ("HTTP/1.1 200 OK", "frontend/script.js")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "frontend/404.html")
+    };
+    let contents = fs::read_to_string(filename).unwrap();
+    let length = contents.len();
+    let response = format!(
+        "{status_line}\r\n\
+        Content-Length: {length}\r\n\r\n
+        {contents}"
+    );
+    stream.write_all(response.as_bytes()).unwrap();
 }
